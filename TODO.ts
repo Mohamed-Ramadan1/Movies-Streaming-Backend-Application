c6b12c4a19e8f8a1a9be1616d6d83fa34f2fb3c1
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2024-09-05 14:14:49 +0300

    add some idea for  the project

--- TODO.ts
+++ TODO.ts
@@ -1,4 +1,24 @@
 /*
+TODO: Auth system  include google auth 
+TODO: Auth system  include  email auth
+TODO: USER Profile 
+TODO: Admin mange users 
+
+
+TODO: Movies Feature
+TODO: Series Feature
+
+TODO: Subscriptions Feature (multi model)
+
+TODO: Reviews and Ratings Feature
+
+TODO: Discussion Feature
+
+TODO: Notifications Feature(realtime  )
+TODO: Email Notification Feature
+
+TODO: Analytics Feature
+
 
 
 

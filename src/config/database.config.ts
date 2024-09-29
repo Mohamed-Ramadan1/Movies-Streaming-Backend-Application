48b1c4e78fcd8e771190f20594f67cfaace49ed0
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2024-09-29 07:00:09 +0300

    setup  database connection and connect database successsfully

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
 // src/config/database.ts
 import { Pool } from "pg";
-import { user, password, host, port, database } from "./index";
+import { user, password, host, port, database } from "./index.config";
 
 const pool = new Pool({
   user,
@@ -11,9 +11,17 @@ const pool = new Pool({
 });
 
 const checkDbConnection = async () => {
+  console.log("Connecting to DB with:", {
+    user,
+    host,
+    port,
+    database,
+  });
+
   try {
-    await pool.connect();
+    const client = await pool.connect();
     console.log("Database connected successfully");
+    client.release(); // Release the connection back to the pool
   } catch (error: any) {
     console.error("Database connection failed:", error.message);
     process.exit(1); // Exit the process with failure code
